4b9895c060f9a4e7a47feac46180b292f3174c74
rogeritael <rogeritael.contato@gmail.com>
2023-11-13 12:17:52 -0300

    ajustando mapa e descrições do porjeto

--- backend/app/controllers/UserController.js
+++ backend/app/controllers/UserController.js
@@ -2,20 +2,24 @@ const UserRepository = require('../repositories/UserRepository')
 
 class UserController {
     async login(req, res){
-        
-        const { username, password } = req.body
-        
-        const user = await UserRepository.findByUserName(username)
-        
-        if(!user){
-            return res.status(404).json({message: 'Usuário não encontrado'})
-        }
-        
-        if(user.password != password){
-            return res.status(404).json({message: 'Senha incorreta'})
-        }
+        try {
 
-        res.status(200).json(user.token)  
+            const { username, password } = req.body
+            
+            const user = await UserRepository.findByUserName(username)
+            
+            if(!user){
+                return res.status(404).json({message: 'Usuário não encontrado'})
+            }
+            
+            if(user.password != password){
+                return res.status(404).json({message: 'Senha incorreta'})
+            }
+
+            res.status(200).json(user.token) 
+        } catch(err){
+            res.status(400).json({ message: 'Erro ao logar' })
+        }
     }
 
     async validateToken(req, res){
